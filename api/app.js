8a87ee2d608f5f20e5e7fc464895becc83de5c50
dntrieu-nguyen <nguyen.dang@rikai.technology>
2024-10-29 14:45:33 +0700

    feature/ui-login

--- api/app.js
+++
@@ -1,9 +0,0 @@
-import request from "@/lib/request";
-
-// export const getInfor = async (data) =>
-//   await request({
-//     method: "GET",
-//     data,
-//   });
-
-// export const getData = async (fields, id) => await getDoc(fields, id);
